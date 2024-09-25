894d4a6c9b8423f61a3fff704cd2c007c8063e8d
LeTon25 <99533245+LeTon25@users.noreply.github.com>
2024-09-25 15:52:29 +0700

    add entity for booking

---
+++ Services/Booking.API/Entities/BookingRoom.cs
@@ -0,0 +1,18 @@
+﻿using Contracts.Domains;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Booking.API.Entities
+{
+    [Table("BookingRooms")]
+    public class BookingRoom : EntityBase<int>
+    {
+        public string UserId { get; set; }
+        public DateTime? CheckIn { get; set; }
+        public DateTime? CheckOut { get; set; }
+        public double PriceTotal { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }    
+        public int NumberOfPeople { get; set; }
+        public ICollection<DetailBookingRoom>?  DetailBookingRooms { get; set; }    
+    }
+}
---
+++ Services/Booking.API/Persistence/BookingDbContextSeeder.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ILogger = Serilog.ILogger;
+
+namespace Booking.API.Persistence
+{
+    public class BookingDbContextSeeder
+    {
+        private readonly BookingDbContext _context;
+        private readonly ILogger _logger;
+        public BookingDbContextSeeder(BookingDbContext context, ILogger logger)
+        {
+            this._context = context;
+            this._logger = logger;
+        }
+        public async Task InitialiseAsync()
+        {
+            try
+            {
+                _logger.Information("Initializing Db");
+                if (_context.Database.IsMySql())
+                {
+                    await _context.Database.MigrateAsync();
+                    _logger.Information("Initialed Db");
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.Error($"Failed to Initialise database :{ex.Message}");
+            }
+        }
+    }
+}
