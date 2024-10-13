f49621e1444f3e6219b70e95013f1aac248de805
Bien Thanh Hung <hungbienthanh@gmail.com>
2024-10-13 22:02:05 +0700

    done tour with schedules

--- Services/Tour.API/Repositories/Services/Interfaces/IScheduleService.cs
+++ Services/Tour.API/Repositories/Services/Interfaces/IScheduleService.cs
@@ -7,7 +7,8 @@ namespace Tour.API.Services.Interfaces
     {
         Task<ApiResponse<List<ScheduleResponseDTO>>> GetAllAsync();
         Task<ApiResponse<ScheduleResponseDTO>> GetByIdAsync(int id);
-        Task<ApiResponse<int>> CreateAsync(ScheduleRequestDTO item);
+        Task<ApiResponse<List<ScheduleResponseDTO>>> GetByTourIdAsync(int tourId);
+		Task<ApiResponse<int>> CreateAsync(ScheduleRequestDTO item);
         Task<ApiResponse<ScheduleResponseDTO>> UpdateAsync(ScheduleRequestDTO item);
         Task<ApiResponse<int>> DeleteAsync(int id);
     }
