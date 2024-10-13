4f50b48da05956f90781c086fdedd7db6836c770
nguyen huy <bihuy027@gmail.com>
2024-10-13 19:24:31 +0700

    fix bugs

--- BuildingBlocks/Shared/DTOs/VideosDTO.cs
+++ BuildingBlocks/Shared/DTOs/VideosDTO.cs
@@ -2,7 +2,7 @@
 {
 	public class VideosDTO
 	{
-		public int Id { get; set; } = 0;
+		public string Id { get; set; }
 		public string Url { get; set; } = string.Empty;
 		public string Type { get; set; } = string.Empty;
 	}
