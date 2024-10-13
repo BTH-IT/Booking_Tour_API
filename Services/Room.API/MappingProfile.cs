722e05c8cddbffa3588f45e5b1f09a0378b77485
An-HDT <thaian111203@gmail.com>
2024-10-13 21:51:31 +0700

    Fixs

--- Services/Room.API/MappingProfile.cs
+++ Services/Room.API/MappingProfile.cs
@@ -1,5 +1,4 @@
 ﻿using AutoMapper;
-using Google.Protobuf.WellKnownTypes;
 using Newtonsoft.Json;
 using Room.API.Entities;
 using Shared.DTOs;
@@ -12,64 +11,135 @@ namespace Room.API
 		{
 			CreateMap<Hotel, HotelResponseDTO>()
 				.ForMember(dest => dest.Reviews, opt =>
-					opt.MapFrom(src => JsonConvert.DeserializeObject<List<ReviewHotelDTO>>(src.Reviews)))
+				{
+					opt.PreCondition(src => src.Reviews != null); 
+					opt.MapFrom(src => JsonConvert.DeserializeObject<List<ReviewHotelDTO>>(src.Reviews));
+				})
 				.ForMember(dest => dest.HotelRules, opt =>
-					opt.MapFrom(src => JsonConvert.DeserializeObject<List<HotelRulesDTO>>(src.HotelRules)))
+				{
+					opt.PreCondition(src => src.HotelRules != null);
+					opt.MapFrom(src => JsonConvert.DeserializeObject<List<HotelRulesDTO>>(src.HotelRules));
+				})
 				.ForMember(dest => dest.HotelAmenities, opt =>
-					opt.MapFrom(src => JsonConvert.DeserializeObject<List<HotelAmenitiesDTO>>(src.HotelAmenities)))
+				{
+					opt.PreCondition(src => src.HotelAmenities != null);
+					opt.MapFrom(src => JsonConvert.DeserializeObject<List<HotelAmenitiesDTO>>(src.HotelAmenities));
+				})
 				.ReverseMap()
 				.ForMember(dest => dest.Reviews, opt =>
-					opt.MapFrom(src => JsonConvert.SerializeObject(src.Reviews)))
+				{
+					opt.PreCondition(src => src.Reviews != null);
+					opt.MapFrom(src => JsonConvert.SerializeObject(src.Reviews));
+				})
 				.ForMember(dest => dest.HotelRules, opt =>
-					opt.MapFrom(src => JsonConvert.SerializeObject(src.HotelRules)))
+				{
+					opt.PreCondition(src => src.HotelRules != null);
+					opt.MapFrom(src => JsonConvert.SerializeObject(src.HotelRules));
+				})
 				.ForMember(dest => dest.HotelAmenities, opt =>
-					opt.MapFrom(src => JsonConvert.SerializeObject(src.HotelAmenities)));
+				{
+					opt.PreCondition(src => src.HotelAmenities != null);
+					opt.MapFrom(src => JsonConvert.SerializeObject(src.HotelAmenities));
+				});
 
 			CreateMap<HotelRequestDTO, Hotel>()
 				.ForMember(dest => dest.HotelRules, opt =>
-					opt.MapFrom(src => JsonConvert.SerializeObject(src.HotelRules)))
+				{
+					opt.PreCondition(src => src.HotelRules != null);
+					opt.MapFrom(src => JsonConvert.SerializeObject(src.HotelRules));
+				})
 				.ForMember(dest => dest.HotelAmenities, opt =>
-					opt.MapFrom(src => JsonConvert.SerializeObject(src.HotelAmenities)))
+				{
+					opt.PreCondition(src => src.HotelAmenities != null);
+					opt.MapFrom(src => JsonConvert.SerializeObject(src.HotelAmenities));
+				})
 				.ReverseMap()
 				.ForMember(dest => dest.HotelRules, opt =>
-					opt.MapFrom(src => JsonConvert.DeserializeObject<List<HotelRulesDTO>>(src.HotelRules)))
+				{
+					opt.PreCondition(src => src.HotelRules != null);
+					opt.MapFrom(src => JsonConvert.DeserializeObject<List<HotelRulesDTO>>(src.HotelRules));
+				})
 				.ForMember(dest => dest.HotelAmenities, opt =>
-					opt.MapFrom(src => JsonConvert.DeserializeObject<List<HotelAmenitiesDTO>>(src.HotelAmenities)));
+				{
+					opt.PreCondition(src => src.HotelAmenities != null);
+					opt.MapFrom(src => JsonConvert.DeserializeObject<List<HotelAmenitiesDTO>>(src.HotelAmenities));
+				});
 
 			CreateMap<RoomEntity, RoomResponseDTO>()
 				.ForMember(dest => dest.Reviews, opt =>
-					opt.MapFrom(src => JsonConvert.DeserializeObject<List<ReviewRoomDTO>>(src.Reviews)))
+				{
+					opt.PreCondition(src => src.Reviews != null);
+					opt.MapFrom(src => JsonConvert.DeserializeObject<List<ReviewRoomDTO>>(src.Reviews));
+				})
 				.ForMember(dest => dest.RoomAmenities, opt =>
-					opt.MapFrom(src => JsonConvert.DeserializeObject<List<RoomAmenitiesDTO>>(src.RoomAmenities)))
+				{
+					opt.PreCondition(src => src.RoomAmenities != null);
+					opt.MapFrom(src => JsonConvert.DeserializeObject<List<RoomAmenitiesDTO>>(src.RoomAmenities));
+				})
 				.ForMember(dest => dest.Images, opt =>
-					opt.MapFrom(src => JsonConvert.DeserializeObject<List<ImagesDTO>>(src.Images)))
+				{
+					opt.PreCondition(src => src.Images != null);
+					opt.MapFrom(src => JsonConvert.DeserializeObject<List<ImagesDTO>>(src.Images));
+				})
 				.ForMember(dest => dest.Video, opt =>
-					opt.MapFrom(src => JsonConvert.DeserializeObject<VideosDTO>(src.Video)))
+				{
+					opt.PreCondition(src => src.Video != null);
+					opt.MapFrom(src => JsonConvert.DeserializeObject<VideosDTO>(src.Video));
+				})
 				.ReverseMap()
 				.ForMember(dest => dest.Reviews, opt =>
-					opt.MapFrom(src => JsonConvert.SerializeObject(src.Reviews)))
+				{
+					opt.PreCondition(src => src.Reviews != null);
+					opt.MapFrom(src => JsonConvert.SerializeObject(src.Reviews));
+				})
 				.ForMember(dest => dest.RoomAmenities, opt =>
-					opt.MapFrom(src => JsonConvert.SerializeObject(src.RoomAmenities)))
+				{
+					opt.PreCondition(src => src.RoomAmenities != null);
+					opt.MapFrom(src => JsonConvert.SerializeObject(src.RoomAmenities));
+				})
 				.ForMember(dest => dest.Images, opt =>
-					opt.MapFrom(src => JsonConvert.SerializeObject(src.Images)))
+				{
+					opt.PreCondition(src => src.Images != null);
+					opt.MapFrom(src => JsonConvert.SerializeObject(src.Images));
+				})
 				.ForMember(dest => dest.Video, opt =>
-					opt.MapFrom(src => JsonConvert.SerializeObject(src.Video)));
+				{
+					opt.PreCondition(src => src.Video != null);
+					opt.MapFrom(src => JsonConvert.SerializeObject(src.Video));
+				});
 
 			CreateMap<RoomRequestDTO, RoomEntity>()
 				.ForMember(dest => dest.RoomAmenities, opt =>
-					opt.MapFrom(src => JsonConvert.SerializeObject(src.RoomAmenities)))
+				{
+					opt.PreCondition(src => src.RoomAmenities != null);
+					opt.MapFrom(src => JsonConvert.SerializeObject(src.RoomAmenities));
+				})
 				.ForMember(dest => dest.Images, opt =>
-					opt.MapFrom(src => JsonConvert.SerializeObject(src.Images)))
+				{
+					opt.PreCondition(src => src.Images != null);
+					opt.MapFrom(src => JsonConvert.SerializeObject(src.Images));
+				})
 				.ForMember(dest => dest.Video, opt =>
-					opt.MapFrom(src => JsonConvert.SerializeObject(src.Video)))
-			.ReverseMap()
+				{
+					opt.PreCondition(src => src.Video != null);
+					opt.MapFrom(src => JsonConvert.SerializeObject(src.Video));
+				})
+				.ReverseMap()
 				.ForMember(dest => dest.RoomAmenities, opt =>
-					opt.MapFrom(src => JsonConvert.DeserializeObject<List<RoomAmenitiesDTO>>(src.RoomAmenities)))
+				{
+					opt.PreCondition(src => src.RoomAmenities != null);
+					opt.MapFrom(src => JsonConvert.DeserializeObject<List<RoomAmenitiesDTO>>(src.RoomAmenities));
+				})
 				.ForMember(dest => dest.Images, opt =>
-					opt.MapFrom(src => JsonConvert.DeserializeObject<List<Image>>(src.Images)))
+				{
+					opt.PreCondition(src => src.Images != null);
+					opt.MapFrom(src => JsonConvert.DeserializeObject<List<Image>>(src.Images));
+				})
 				.ForMember(dest => dest.Video, opt =>
-					opt.MapFrom(src => JsonConvert.DeserializeObject<Video>(src.Video)));
-
+				{
+					opt.PreCondition(src => src.Video != null);
+					opt.MapFrom(src => JsonConvert.DeserializeObject<Video>(src.Video));
+				});
 
 			CreateMap<ReviewHotelDTO, ReviewHotel>();
 			CreateMap<ReviewHotel, ReviewHotelDTO>();
