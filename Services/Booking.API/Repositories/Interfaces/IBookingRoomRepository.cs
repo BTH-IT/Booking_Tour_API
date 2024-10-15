55b6c89202abe096cc03d7bcdf0994c688a8194b
An-HDT <thaian111203@gmail.com>
2024-10-15 14:07:59 +0700

    fix video in tour

---
+++ Services/Booking.API/Repositories/Interfaces/IBookingRoomRepository.cs
@@ -0,0 +1,15 @@
+﻿using Booking.API.Entities;
+using Booking.API.Persistence;
+using Contracts.Domains.Interfaces;
+
+namespace Booking.API.Repositories.Interfaces
+{
+	public interface IBookingRoomRepository : IRepositoryBase<BookingRoom, int, BookingDbContext>
+	{
+		Task<IEnumerable<BookingRoom>> GetBookingRoomsAsync();
+		Task<BookingRoom> GetBookingRoomByIdAsync(int id);
+		Task CreateBookingRoomAsync(BookingRoom bookingRoom);
+		Task UpdateBookingRoomAsync(BookingRoom bookingRoom);
+		Task DeleteBookingRoomAsync(int id);
+	}
+}
