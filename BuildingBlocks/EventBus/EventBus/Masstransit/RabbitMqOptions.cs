3fa72237d6a6dff879ea2d64e22a1f51912d03c1
LeTon25 <99533245+LeTon25@users.noreply.github.com>
2024-11-10 14:32:05 +0700

    fix identity api

--- BuildingBlocks/EventBus/EventBus/Masstransit/RabbitMqOptions.cs
+++ BuildingBlocks/EventBus/EventBus/Masstransit/RabbitMqOptions.cs
@@ -1,17 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 
 namespace EventBus.Masstransit
 {
     public class RabbitMqOptions
     {
-        public string HostName { get; set; }
-        public string ExchangeName { get; set; }
-        public string UserName { get; set; }
-        public string Password { get; set; }
+        public string HostName { get; set; } = string.Empty;
+        public string ExchangeName { get; set; } = string.Empty;
+        public string UserName { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
         public ushort Port { get; set; }    
     }
 }
---
+++ Services/Realtime.API/Realtime.API/Program.cs
@@ -0,0 +1,69 @@
+using EventBus.Masstransit;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.OpenApi.Models;
+using Realtime.API.Extensions;
+using Realtime.API.Hubs;
+using Serilog;
+var builder = WebApplication.CreateBuilder(args);
+
+Log.Information($"Start {builder.Environment.ApplicationName} up");
+
+try
+{
+    builder.AddAppConfigurations();
+    // Add services to the container.
+    builder.Services.AddControllers();
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+    builder.Services.Configure<ApiBehaviorOptions>(options =>
+    {
+        options.SuppressModelStateInvalidFilter = true;
+    });
+    builder.Services.AddCustomMassTransit(builder.Environment,typeof(Program).Assembly);
+    // Configure Route Options 
+    builder.Services.Configure<RouteOptions>(cfg => cfg.LowercaseQueryStrings = true);
+    // Add CORS
+    builder.Services.ConfigureCors(builder.Configuration);
+
+    //Add Swagger Gen
+    builder.Services.AddSwaggerGen(
+        options =>
+        {
+            options.SwaggerDoc("v1", new OpenApiInfo { Title = "BookingSystem - Realtime API", Version = "v1" });
+        }
+    );
+    builder.Services.AddSignalR();
+    // Configure the HTTP request pipeline.
+    var app = builder.Build();
+
+    if (app.Environment.IsDevelopment() || app.Environment.IsEnvironment("docker"))
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+    app.UseCors("CorsPolicy");
+
+    app.UseAuthorization();
+
+    app.MapHub<NotiHub>("/api/noti");
+    app.MapControllers();
+  
+    app.Run();
+
+}
+catch (Exception ex)
+{
+    string type = ex.GetType().Name;
+    if (type.Equals("HostAbortedException", StringComparison.Ordinal)) throw;
+
+    Log.Fatal(ex, $"Unhandled exception: {ex.Message}");
+}
+finally
+{
+    Log.Information($"Shut down {builder.Environment.ApplicationName} complete");
+    Log.CloseAndFlush();
+}
+
+
+
