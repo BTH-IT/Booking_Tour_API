856819ffebfdc579af54275b22efef907b5c322c
LeTon25 <99533245+LeTon25@users.noreply.github.com>
2024-11-10 14:32:05 +0700

    fix identity api

--- BuildingBlocks/EventBus/EventBus/IntergrationEvents/Events/IntergrationEvent.cs
+++ BuildingBlocks/EventBus/EventBus/IntergrationEvents/Events/IntergrationEvent.cs
@@ -1,13 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using EventBus.IntergrationEvents.Interfaces;
+﻿using EventBus.IntergrationEvents.Interfaces;
 
 namespace EventBus.IntergrationEvents.Events
 {
-    public record IntergrationEvent() : IIntergrationEvent
+    public class IntergrationEvent : IIntergrationEvent
     {
         public DateTime CreationDate { get; set; }
         public Guid Id { get; set; }
---
+++ Services/Realtime.API/Realtime.API/Extensions/ServiceExtension.cs
@@ -0,0 +1,25 @@
+﻿using Shared.Configurations;
+namespace Realtime.API.Extensions
+{
+    public static class ServiceExtension
+    {
+        public static IServiceCollection AddConfigurationSettings(this IServiceCollection services,IConfiguration configuration)
+        {
+            var grpcSettings = configuration.GetSection(nameof(GrpcSettings));
+            services.AddSingleton(grpcSettings);
+            return services;
+        }
+
+        public static IServiceCollection ConfigureCors(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddCors(option =>
+                option.AddPolicy("CorsPolicy", cfg =>
+                    cfg.WithOrigins("*")
+                        .AllowAnyHeader()
+                        .AllowAnyMethod()
+                )
+            );
+            return services;
+        }
+    }
+}
